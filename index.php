cd0b858afc8276c43037e54daf484abfc005a11c
piotrlipert <piotr.lipert@gmail.com>
2014-05-31 01:50:16 +0200

    as
    as

--- index.php
+++ index.php
@@ -1,8 +1,25 @@
 
 <head>
 </head>
+
 <body style="background-color:#ACDBCE;">
 
 
-<img src="back.jpg">
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="jquery.rwdImageMaps.min.js"></script>
+<script>
+$(document).ready(function(e) {
+	$('img[usemap]').rwdImageMaps();
+	
+	$('area').on('click', function() {
+		alert($(this).attr('alt') + ' clicked');
+	});
+});
+
+</script>
+
+
 </body>
+
