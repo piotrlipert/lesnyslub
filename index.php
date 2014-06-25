f483fb145c04211b22a13d4f3698acd7e442caf5
piotrlipert <piotr.lipert@gmail.com>
2014-06-25 20:06:37 +0200

    dadas
    dsa

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 		<div>	
 		<img src="img/6.png" ALT="image" style="max-width:100%;height:auto;">
 		</div>
-		</div>
+	</div>
 
 		<div style="float:left;margin:0;position:relative;top:37%;left:18%; ">
 				<input type="image" src="img/3.png" alt="Submit">
@@ -44,7 +44,7 @@
   <br><br>
 	
 	
-<div style="text-align:center;background-color: #000000;">
+<div style="text-align:center;background-color: #000000;height:500px">
 
 <a href="https://www.google.com/maps/dir/Zosia.+Dom+wycieczkowy,+32-047+Ojc%C3%B3w;+Z%C5%82ota+G%C3%B3ra+4,+Polska/Willa+Decjusza,+Ul.+28+Lipca+1943,+Kr%C4%85k%C3%B3w,+Polska/@50.1465064,19.7868173,12z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x4716f8bb7fd4899b:0x58ccb62c91df8d6d!2m2!1d19.818112!2d50.211315!1m5!1m1!1s0x47165be6772736c3:0x3ed0468dd04aa42f!2m2!1d19.871778!2d50.062924"><img src="img/dule.png" ALT="image" style="max-width:100%;height:auto;"></a>
 
