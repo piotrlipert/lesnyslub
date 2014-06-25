47b910fe648dfaff8259540ff9014100de36af36
piotrlipert <piotr.lipert@gmail.com>
2014-06-25 19:25:30 +0200

    sasa
    sa

--- scripts/sm.php
+++ scripts/sm.php
@@ -1,6 +1,9 @@
 <?php
 		$message = $_REQUEST['message'];
-
-		echo($message);
+	$myFile = "../potwierdzenia.txt";
+	$fh = fopen($myFile, 'a') or die("can't open file");
+	$stringData = $message;
+	fwrite($fh, $stringData);
+	fclose($fh);
 
 ?>
