3a168f4d8a315c8ccb08e9a0873094a9b6563f81
piotrlipert <piotr.lipert@gmail.com>
2014-06-25 19:56:47 +0200

    sada
    dsa

--- index.php
+++ index.php
@@ -38,13 +38,13 @@
 	</form>
 
 	      <div id="loading" style="display: none;text-align:center;"> 
-    Potwierdzono!
+    <b>Potwierdzono!</b>
     </div>
   
   <br><br>
 	
 	
-<div style="text-align:center;background-color: #000000;height:auto;">
+<div style="text-align:center;background-color: #000000;height:300px">
 
 <a href="https://www.google.com/maps/dir/Zosia.+Dom+wycieczkowy,+32-047+Ojc%C3%B3w;+Z%C5%82ota+G%C3%B3ra+4,+Polska/Willa+Decjusza,+Ul.+28+Lipca+1943,+Kr%C4%85k%C3%B3w,+Polska/@50.1465064,19.7868173,12z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x4716f8bb7fd4899b:0x58ccb62c91df8d6d!2m2!1d19.818112!2d50.211315!1m5!1m1!1s0x47165be6772736c3:0x3ed0468dd04aa42f!2m2!1d19.871778!2d50.062924"><img src="img/dule.png" ALT="image" style="max-width:100%;height:auto;"></a>
 
@@ -61,7 +61,7 @@
 <br>
 <div style="text-align:center;>
 
-<img src="img/5.png" ALT="image" style="max-width:100%;height:auto;">
+<img src="img/5.png" ALT="image" style="max-width:100%;">
 
 </div>
 
